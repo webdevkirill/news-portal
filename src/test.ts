a152f3e09d8b1863fd0186ed66c312dd46a18f7d
webdevkirill <krill.fyodorov@gmail.com>
2023-01-29 16:31:54 +0300

    add webpack configuration

---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export const someFn = (arg: number): string => {
+  console.log('test export');
+
+  return arg.toString();
+}
---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src', 'index.ts'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+  ]
+}
+
+export default config;
