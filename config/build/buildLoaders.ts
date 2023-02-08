6fedd0f5b9ed48f1a42598fae1ca24fb87b2e9b7
webdevkirill <krill.fyodorov@gmail.com>
2023-02-08 19:38:17 +0300

    add babel with i18next-extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -40,5 +40,25 @@ export const buildLoaders = ({ isDev }: IBuildOptions): webpack.RuleSetRule[] =>
     ],
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  const babelLoader = {
+    test: /\.jsx?|ts?x$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 };
