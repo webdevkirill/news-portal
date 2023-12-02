717947797dcf26d1cea686dde587019495523db3
webdevkirill <krill.fyodorov@gmail.com>
2023-12-02 15:59:30 +0300

    fix modal stories

--- src/shared/ui/Modal/ui/Modal.stories.tsx
+++ src/shared/ui/Modal/ui/Modal.stories.tsx
@@ -12,7 +12,7 @@ export default {
   args: {
     isOpen: true,
     children:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse repellat vero modi ullam, dignissimos enim culpa a explicabo quasi veritatis officia sunt amet nobis magnam aut atque tempore non suscipit illum. Laboriosam eum in sapiente distinctio minima excepturi tenetur, corporis sed quo? Obcaecati maiores distinctio aliquam quos fuga doloremque provident ut quia earum! Aliquam cumque quam eligendi nesciunt facere dolores perferendis corrupti mollitia assumenda commodi similique, libero, veniam iste quae quod quibusdam enim provident ipsa? Quam, hic dolorum? Nisi quos atque earum fugiat provident, laboriosam quis aliquid aliquam hic a corrupti facilis. Commodi, quidem necessitatibus placeat autem adipisci ab laborum.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repudiandae quibusdam provident, fugiat quod deleniti sunt eligendi vitae quasi quaerat! ',
   },
 } as ComponentMeta<typeof Modal>;
 
